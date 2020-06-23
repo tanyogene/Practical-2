34b4b28e458c81f8cac87a8b384e78132d583f53
Lenovo <Lenovo@DESKTOP-AF13MFN>
2020-06-23 09:13:34 +0800

    practical 2

---
+++ Practical2/src/Practical2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Practical2 {
+    
+     public static void main (String[] args) {
+  
+       
+    	Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Number SquareRoot");
+        
+        for(int i=0; i<=20;i++){
+     
+        System.out.printf("%-9d %.4f\n",i ,Math.sqrt(i)) ;
+        
+        
+        }
+        
+}
+}
