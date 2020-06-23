b2da2d362747827476288ec9a86fbc51e3b504a9
Lenovo <Lenovo@DESKTOP-AF13MFN>
2020-06-23 09:47:41 +0800

    practical 2

--- Practical2/src/Practical2.java
+++ Practical2/src/Practical2.java
@@ -1,6 +1,8 @@
+
 import java.util.Scanner;
 
-public class Practical2 {
+//Q1
+/*public class Practical2 {
     
      public static void main (String[] args) {
   
@@ -17,4 +19,54 @@ public class Practical2 {
         }
         
 }
+}*/
+//Q2
+public class Practical2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Number    SquareRoot");
+
+        for (int i = 0; i <= 20; i++) {
+
+            System.out.printf("%-9d %.4f\n", i, sqrt(i));
+
+        }
+
+    }
+
+    public static double sqrt(double x) {
+
+        double l=0, u=0;
+        double mid, smid;
+
+        if (x == 0 || x == 1) {
+            return x;
+        } else if (x < 0) {
+            System.out.println("Error:.......");
+            System.exit(-1);
+        } else if (x > 1) {
+            l = 1;
+            u = x;
+        } else {
+            //x<1 && x>0
+            u = 1;
+            l = x;
+        }
+
+        while ( Math.abs((l-u)/l) >= 1.0e-8) {
+            mid = (l + u) / 2;
+            smid = mid * mid;
+
+            if (smid > x) {
+                u = mid;
+            } else {
+                l = mid;
+            }
+        }
+        return(l+u)/2.0;
+    }
+    
 }
